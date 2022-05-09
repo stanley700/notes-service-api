0ac833bc5afbb758aba0e586db95f88673cd43ea
stanley okpala <stanley700@outlook.com>
2022-05-09 03:15:22 +0100

    Unit Testing Prject

---
+++ NotesService.API/NotesService.API.Tests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NotesService.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesService.API.Controllers.Tests
+{
+    [TestClass()]
+    public class UsersControllerTests
+    {
+        //Todo:: Write unit tests
+        [TestMethod()]
+        public void CreateUserTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
